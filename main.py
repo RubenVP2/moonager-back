daad973cfd0e7700f4e929f6ca449ef7cf295806
Sammy ETUR <s@etur.fr>
2021-10-19 09:16:35 +0200

    minimal front for backend test

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-from typing import Optional
-
 from fastapi import FastAPI
+
+from torrent import test_dl_billie
+from torrent import test_rm_all
+from tmdb import popular
+
 
 app = FastAPI()
 
+import json
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    r=popular("tv", "569760ff55e24c593b9cf89e8503decd")
+    print(r["1"].id)
+    return {"r:": "test"}
+
+@app.get("/rm")
+def read_root():
+    test_rm_all()
 
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/top/{media}/{top}")
+def read_item(media: str, top: int):
+    r=popular(media, "569760ff55e24c593b9cf89e8503decd")
+    return {
+        "top": top,
+        "name": r[format(top)].title,
+        "id": r[format(top)].id,
+    }
