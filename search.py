e9c5533948b188e8448cde95c4865a27bff6f162
Sammy ETUR <s@etur.fr>
2021-10-22 10:44:02 +0200

    define class + rework findTorrent function

--- search.py
+++ search.py
@@ -1,43 +1,54 @@
+from os import stat_result
 import feedparser
+from pydantic.main import BaseModel
 import requests
 import re
+from typing import Optional
 
 
-class Torrent:
-    def __init__(self, name, cat, size, seeders, url, enc, res):
-        self.name = name
-        self.cat = cat
-        self.size = size
-        self.seeders = seeders
-        self.url = url
-        self.enc = enc
-        self.res = res
+class TorrentRequest(BaseModel):
+    query: str
+    mediaType: str
+    sizeMax: Optional[int] = 15000000000
+    encoding: Optional[str] = "264"
+    resolution: Optional[str] = "1080"
 
-def findTorrent(q, cat, enc, res, sizeMax):
-    if cat == "tv":
-        cat = "102184"
-    elif cat == "movie":
-        cat = "102183"
-    if enc is None:
-        enc=""
-    if res is None:
-        res=""
-    if sizeMax is None:
-        sizeMax=""
-    q = q.replace(" ", "+")
-    url = "https://jk.etur.fr/api/v2.0/indexers/yggcookie/results/torznab/api?apikey=pfpk2qtgiik9dvctqxpk54txn58vyudf&t=search&cat="+cat+"&q="+q
-    news_feed = feedparser.parse(url)
-    n, t = 0, {}
-    response = requests.get(url)
-    for entry in news_feed.entries:
-        seed = response.text.split("<item>")[n+1].split("seeders")[1].split("\"")
-        if len(seed) > n:
-            if re.search(enc, entry.title):
-                if re.search(res, entry.title):
-                    if int(entry.size) < int(sizeMax):
-                        t = Torrent(entry.title, cat, entry.size, seed[2], entry.link, enc, res)
-                            #print(" Name: "+t.name+" Size: "+t.size+" Seeders: "+t.seeders+" URL: "+t.url)
-        n = n+1
-    return t
 
-#findTorrent("Venom", "movie", "x264", "1080p", "100000000000000000")
+class TorrentFind(BaseModel):
+    title: str
+    category: str
+    size: int
+    seeders: int
+    url: str
+
+
+def findTorrent(request):
+    regex = "^"
+    if request.mediaType == "tv":
+        request.mediaType = "102184"
+    elif request.mediaType == "movie":
+        request.mediaType = "102183"
+    if request.encoding is not None:
+        regex = regex+"(?=.*"+request.encoding+".*)"
+    if request.resolution is not None:
+        regex = regex+"(?=.*"+request.resolution+".*)"
+    request.query = request.query.replace(" ", "+")
+    url = "https://jk.etur.fr/api/v2.0/indexers/yggcookie/results/torznab/api?apikey=pfpk2qtgiik9dvctqxpk54txn58vyudf&t=search&cat=" + \
+        request.mediaType+"&q="+request.query
+    print(url)
+    result = feedparser.parse(url)
+    ResponseSeeders = requests.get(url)
+    torrent = {}
+    for index, entry in enumerate(result.entries):
+        seeders = ResponseSeeders.text.split(
+            "<item>")[index+1].split("seeders")[1].split("\"")[2]
+        if len(entry) > index:
+            if re.search(regex, entry.title, re.IGNORECASE):
+                if int(entry.size) < int(request.sizeMax):
+                    torrent[index] = TorrentFind(title=entry.title, category=request.mediaType, size=entry.size, seeders=seeders, url=entry.link)
+    return torrent
+
+
+#request = TorrentRequest(query="Venom", mediaType="movie",
+#                         encoding="x264", resolution="1080p", sizeMax="100000000000000000")
+#print(findTorrent(request))
