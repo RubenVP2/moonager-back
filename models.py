b60ae59b8e25d12833fbbe34cc465ef4ebb49e60
Sammy ETUR <s@etur.fr>
2021-10-22 16:39:43 +0200

    add example for dev UI of FASTAPI

--- models.py
+++ models.py
@@ -7,7 +7,6 @@ from database import Base
 
 class Film(Base):
     __tablename__ = "films"
-
     id_imdb = Column(String, primary_key=True, unique=True, index=True)
     hash_torrent = Column(String, unique=True, index=True)
 
@@ -17,6 +16,14 @@ class TMDBRequest(BaseModel):
     apiKey: str
     lang: Optional[str] = "en-US"
 
+    class Config:
+        schema_extra = {
+            "example": {
+                "mediaType": "movie",
+                "apiKey": "569760ff55e24c593b9cf89e8503decd"
+                }
+        }
+
 
 class TMDBSearch(TMDBRequest):
     query: Optional[str] = None
@@ -24,6 +31,8 @@ class TMDBSearch(TMDBRequest):
     class Config:
         schema_extra = {
             "example": {
+                "mediaType": "movie",
+                "apiKey": "569760ff55e24c593b9cf89e8503decd",
                 "query": "Deadpool 2"
             }
         }
@@ -31,10 +40,16 @@ class TMDBSearch(TMDBRequest):
 
 class TMBDContent(TMDBRequest):
     id: int
-    name: Optional[str] = None
 
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": 383498
+            }
+        }
 
 class TMDBMovieInfo(TMBDContent):
+    title: str
     backdrop: str
     poster: str
     genres: dict
@@ -57,6 +72,20 @@ class TorrentRequest(BaseModel):
     sizeMax: Optional[int] = 15000000000
     encoding: Optional[str] = "264"
     resolution: Optional[str] = "1080"
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "query": "Deadpool 2",
+                "apiKey": "pfpk2qtgiik9dvctqxpk54txn58vyudf",
+                "host": "jk.etur.fr",
+                "mediaType": "movie",
+                "sizeMax": 15000000000,
+                "encoding": "264",
+                "resolution": "1080"
+            }
+        }
+
 
 
 class TorrentFind(BaseModel):
